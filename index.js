6d553a31f338d99cb229b5970a842c21998a56df
jennifer hodge <jenniferhodge@jennifers-Air.lan>
2023-10-30 18:13:47 -0500

    end of part 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let rs = require('readline-sync');
+let rs = require("readline-sync");
 
 // prompt name question
 // prompt fav hobby
@@ -9,12 +9,39 @@ let rs = require('readline-sync');
 
 let hobbies = [];
 
-// let userName = rs.question('What is your name? ');
-// console.log('Hi '+ userName + '!');
+let userName = rs.question("What is your name? ");
 
-let favHobby = rs.question('What is your favorite hobby? ')
+let favHobby = rs.question(userName + ", what is your favorite hobby? ");
 
 hobbies.push(favHobby);
+
+let moreHobbies = rs.questionInt("How many other hobbies do you have?");
+
+if (moreHobbies > 0) {
+  let counter = 0;
+  while (counter < moreHobbies) {
+    let input = rs.prompt();
+    hobbies.push(input);
+    counter++;
+
+    if (counter < moreHobbies) {
+      console.log(
+        '--Awesome! "' +
+          input +
+          "\" is cool! What's the other " +
+          (moreHobbies - counter) +
+          "?"
+      );
+    } else {
+      console.log("I wish I could do " + hobbies + "...but I'm not even real");
+    }
+  }
+
+  // add hobbies
+} else {
+  let areYouSure = rs.keyInYN(
+    "Are you sure?" + hobbies[0] + " is all you like to do?"
+  );
+}
+
 console.log(hobbies);
-
-
